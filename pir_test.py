50973cff4a9dfeaa1fab7f2d8bf2b1519017574b
Philipp Redeker <philipp@rdkr.com>
2023-04-18 15:28:57 +0200

    test motion and flash LED

--- pir_test.py
+++ pir_test.py
@@ -1,21 +1,27 @@
 import RPi.GPIO as GPIO
 import time
 
-SENSOR_PIN = 26  # Change this to the GPIO pin you are using for the motion sensor
+SENSOR_PIN = 26
+LED_PIN = 6
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 def motion_detected(channel):
     print("Motion detected!")
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_PIN, GPIO.LOW)
 
 GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING, callback=motion_detected)
 
 try:
-    print("PIR motion sensor test. Press Ctrl+C to exit.")
+    print("PIR motion sensor test with LED. Press Ctrl+C to exit.")
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
+    GPIO.output(LED_PIN, GPIO.LOW)
     GPIO.cleanup()
